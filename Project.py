e443c0632412a61471c16db67890a9d1683cd754
Blastzard <142840435+Blastzard@users.noreply.github.com>
2024-03-27 13:17:40 -0500

    joiio9ijofdsioufdiu
    adfdsa

---
+++ Project.py
@@ -0,0 +1,14 @@
+def main():
+    pass
+def get_shift():
+    pass
+def choose_option():
+    pass
+def get_message():
+    pass
+def create_key():
+    pass
+def encode(message,key):
+    pass
+def decode(message,key):
+    pass
